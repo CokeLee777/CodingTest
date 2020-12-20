414cd3ed4f03649f06c20742a5791ab13a7dd533
Lee12312 <akak640@kyonggi.ac.kr>
2020-12-20 21:23:43 +0900

    숫자 카드 게임

---
+++ Chapter 03/numberCardGame.py
@@ -0,0 +1,15 @@
+# 숫자 카드 게임
+
+def numberCardGame(N, M, result):
+    #각 카드에 적인 숫자가 주어진다.
+    for i in range(N):
+        cardlist = list(map(int, input().split()))
+        if result < min(cardlist):    
+            result = min(cardlist)
+        
+    return result
+
+#숫자 카드들이 놓인 행의 개수 N 과 열의갯수 M 이 공백을 기준으로 각각 자연수로 주어짐
+N, M = map(int, input().split())
+
+print(numberCardGame(N,M,0))
