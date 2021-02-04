9015c83c2fcd912abbf09f36a436db23728d457f
Lee12312 <akak640@kyonggi.ac.kr>
2021-02-04 18:40:24 +0900

    codingtest practice

--- Java/src/Chapter08/actualproblem02/Solution.java
+++ Java/src/Chapter08/actualproblem02/Solution.java
@@ -2,35 +2,37 @@ package Chapter08.actualproblem02;
 
 import java.util.*;
 
-public class MySourceCode {
-
+public class Solution {
     public static int[] arr = new int[30001];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //정수 x 를 입력받는다.
+
         int x = sc.nextInt();
 
-        for(int i = 2; i <= x; i++){
-            //현재 값에서 1을 먼저 뺐을때 값
-            arr[i] = arr[i-1] + 1;
-            //5로 나누어 떨어지면
+        for(int i = 2; i < x + 1; i++){
+
+            //x에서 1을 빼는 경우
+            arr[i] = arr[i - 1] + 1;
+
+            //x가 5로 나누어 떨어지면
             if(i % 5 == 0){
                 arr[i] = Math.min(arr[i], arr[i / 5] + 1);
                 continue;
             }
-            //3으로 나누어 떨어지면
+            //x가 3으로 나누어 떨어지면
             if(i % 3 == 0){
                 arr[i] = Math.min(arr[i], arr[i / 3] + 1);
                 continue;
             }
-            //2로 나누어 떨어지면
+            //x가 2로 나누어 떨어지면
             if(i % 2 == 0){
                 arr[i] = Math.min(arr[i], arr[i / 2] + 1);
             }
+
         }
 
-        System.out.println("result = " + arr[x]);
-    }
+        System.out.println(arr[x]);
 
+    }
 }
--- Java/src/Chapter08/actualproblem03/MySourceCode.java
+++ Java/src/Chapter08/actualproblem03/MySourceCode.java
@@ -13,19 +13,19 @@ public class MySourceCode {
 
         //식량창고 갯수 입력받기
         int n = sc.nextInt();
-        //각 식량창고에 저장된 식량의 갯수 입력 받기
+        //식량창고 초기화
         store = new int[n];
-        for(int i = 0; i < n; i++) store[i] = sc.nextInt();
-
-        //다이나믹 프로그래밍 진행 보텀업
+        for(int i = 0; i < n; i++){
+            store[i] = sc.nextInt();
+        }
         arr[0] = store[0];
         arr[1] = Math.max(store[0], store[1]);
+        //다이나믹 프로그래밍 수행 - 보텀업
         for(int i = 2; i < n; i++){
-            //현재 위치까지의 식량창고중에서(3개의 식량창고) 두칸떨어진 식량창고의 합과 중간 식량창고와 비교해서 큰 값으로 값을 삽입
-            arr[i] = Math.max(arr[i - 1], arr[i - 2] + store[i]);
+            arr[i] = Math.max(arr[i-1], store[i] + arr[i-2]);
         }
 
-        System.out.println(arr[n-1]);
+        System.out.println("result = " + arr[n-1]);
     }
 
 }
