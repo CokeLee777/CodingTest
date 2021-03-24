5a877f85f00ce86bc28883c50ea42dabc6816200
Lee12312 <akak640@kyonggi.ac.kr>
2021-03-24 18:12:02 +0900

    codingtest practice

--- Java/src/Chapter08/actualproblem02/MySourceCode.java
+++ Java/src/Chapter08/actualproblem02/MySourceCode.java
@@ -4,33 +4,37 @@ import java.util.*;
 
 public class MySourceCode {
 
-    public static int[] arr = new int[30001];
+    public static int x;    //정수 x
+    public static int[] d;  //다이나믹 테이블
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //정수 x 를 입력받는다.
-        int x = sc.nextInt();
 
+        //정수 x 입력받기
+        x = sc.nextInt();
+        //다이나믹 테이블 초기화
+        d = new int[x+1];
+
+        //보텀업 방식으로 다이나믹 프로그래밍 수행
         for(int i = 2; i <= x; i++){
-            //현재 값에서 1을 먼저 뺐을때 값
-            arr[i] = arr[i-1] + 1;
-            //5로 나누어 떨어지면
-            if(i % 5 == 0){
-                arr[i] = Math.min(arr[i], arr[i / 5] + 1);
-                continue;
-            }
-            //3으로 나누어 떨어지면
-            if(i % 3 == 0){
-                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
-                continue;
-            }
-            //2로 나누어 떨어지면
+            //1을 먼저 뺀 방식을 사용
+            d[i] = d[i-1] + 1;
+
+            //2로 나누어 지는 경우
             if(i % 2 == 0){
-                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+                d[i] = Math.min(d[i], d[i / 2] + 1);
+            }
+            //3으로 나누어 지는 경우
+            if(i % 3 == 0){
+                d[i] = Math.min(d[i], d[i / 3] + 1);
+            }
+            //5로 나누어 지는 경우
+            if(i % 5 == 0){
+                d[i] = Math.min(d[i], d[i / 5] + 1);
             }
         }
 
-        System.out.println("result = " + arr[x]);
+        System.out.println(d[x]);
     }
 
 }
--- Java/src/Chapter08/actualproblem03/MySourceCode.java
+++ Java/src/Chapter08/actualproblem03/MySourceCode.java
@@ -4,28 +4,32 @@ import java.util.*;
 
 public class MySourceCode {
 
-    public static int[] arr = new int[101];
-
-    public static int[] store;
+    public static int[] store;  //식량창고
+    public static int n;        //식량창고의 개수
+    public static int[] d;      //다이나믹 테이블
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //식량창고 갯수 입력받기
-        int n = sc.nextInt();
-        //식량창고 초기화
+        //식량창고의 개수 입력받기
+        n = sc.nextInt();
+        //식량창고에 저장된 식량의 개수 입력받기
         store = new int[n];
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < store.length; i++){
             store[i] = sc.nextInt();
         }
-        arr[0] = store[0];
-        arr[1] = Math.max(store[0], store[1]);
-        //다이나믹 프로그래밍 수행 - 보텀업
+        //다이나믹 테이블 초기화
+        d = new int[n];
+        //첫번째 두번째 까지의 다이나믹 테이블 초기화
+        d[0] = store[0];
+        d[1] = Math.max(store[0], store[1]);
+
+        //세번째 식량창고부터의 다이나믹 프로그래밍 수행
         for(int i = 2; i < n; i++){
-            arr[i] = Math.max(arr[i-1], store[i] + arr[i-2]);
+            d[i] = Math.max(d[i-2] + store[i], d[i-1]);
         }
 
-        System.out.println("result = " + arr[n-1]);
+        System.out.println(d[n-1]);
     }
 
 }
